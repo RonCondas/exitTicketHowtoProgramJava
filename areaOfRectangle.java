86234ccf5ac34a1151f5554944e24e379440285f
RonCondas <124211322+RonCondas@users.noreply.github.com>
2023-10-10 11:33:50 -0500

    Add files via upload

---
+++ areaOfRectangle.java
@@ -0,0 +1,85 @@
+//package areaRectangle;
+//
+//import java.util.Scanner;
+//
+//public class areaOfRectangle {
+//
+//	public static void main(String[] args) {
+//		
+//		//1.Variables
+//		double length1 = 0;
+//		double width1 = 0;
+//		double area1 = 0.0;
+//		double length2 = 0;
+//		double width2 = 0;
+//		double area2 = 0.0;
+//		
+//		double length1 =getLength1();
+//		
+//	
+//		
+//		//5.Display
+//		
+//		System.out.println("Length 1:" + length1);
+//		System.out.println("width 1:" + width1);
+//		System.out.println("width 2:" + width1);
+//		System.out.println("Length 2:" + width2);
+//		System.out.println("area 1:" + area1);
+//		System.out.println("area 2:" + area2);
+//	
+//	
+//	} public static double getLength1() {
+//		
+//		Scanner kb = new Scanner(System.in);
+//		//1.Variables
+//		
+//		
+//		//2.Ask the user for the length
+//		System.out.println("Please enter length 1: ");
+//		
+//		
+//		return kb.nextDouble();
+//	}
+//	// for (int i )
+//      public static double getWidth1() {
+//		
+//		Scanner kb = new Scanner(System.in);
+//		//1.Variables
+//		double width1 =0;
+//		
+//		//2.Ask the user for the length
+//		System.out.println("Please enter width 1: ");
+//		//3.Store
+//		width1 = kb.nextDouble();
+//		
+//		return width1;
+//		}public static double getWidth2() {
+//			
+//			Scanner kb = new Scanner(System.in);
+//			//1.Variables
+//			double width2 =0;
+//			
+//			//2.Ask the user for the length
+//			System.out.println("Please enter length 1: ");
+//			//3.Store
+//			width2 = kb.nextDouble();
+//			
+//			return width2;
+//		}
+//		// for (int i )
+//	      public static double getlength2() {
+//			
+//			Scanner kb = new Scanner(System.in);
+//			//1.Variables
+//			double length2 =0;
+//			
+//			//2.Ask the user for the length
+//			System.out.println("Please enter width 1: ");
+//			//3.Store
+//			length2 = kb.nextDouble();
+//			
+//			return length2;
+//		
+//	     }
+//
+//} 		
